d367737c4f71feb9b958a4231eca8de4b2cc78bd
Talles Alves <talves2503@gmail.com>
2024-11-07 18:08:31 -0300

    feat: add post expense endpoint

---
+++ src/CashFlow.API/Controllers/ExpensesController.cs
@@ -0,0 +1,14 @@
+﻿using CashFlow.Communication.Requests;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CashFlow.API.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class ExpensesController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult Register([FromBody] RequestExpense request)
+    {
+        return Created();
+    }
+}
